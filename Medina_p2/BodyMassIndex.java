13568956bf7a249c9783ba556e89af3168b4e5a6
Eduardo J M <70599516+Bjorngott@users.noreply.github.com>
2020-09-29 00:12:54 -0400

    Update BodyMassIndex.java

--- Medina_p2/BodyMassIndex.java
+++ Medina_p2/BodyMassIndex.java
@@ -9,8 +9,18 @@ public class BodyMassIndex {
     //Calc BMI score
     public double CalcBmi(double height, double weight){
         double index = 703 * weight / Math.pow(height, 2);
-
+        
+        if(index < 18.5){
+            System.out.println("Underweight");
+        }else if(index >= 18.5 && index <= 24.9){
+            System.out.println("Normal Weight");
+        }else if(index >= 25 && index <= 29.9){
+            System.out.println("Overweight");
+        }else{
+            System.out.println("Obese");
+        }
+        
         return index;
-
     }
+    
 }
